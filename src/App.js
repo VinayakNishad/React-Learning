c5df0a32686d2e7bfe7eb66e7eb7c1338a8212dd
VinayakNishad <vn07244@gmail.com>
2025-02-02 13:32:45 +0530

    all

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  let name="Vinayak";
+  let l=[10,23,23,4,45];
+  let obj={
+    name: "binu",
+    age:21
+  }
+  return (
+    <div className='App'>
+      <h1 className='name'>My name is {name}</h1>
+      {
+        l.map((v)=>{
+          return (
+            <div>{v}</div>
+          )
+        })
+      }
+      <h1>{obj.name}</h1>
+    </div>
+  );
+}
+
+export default App;
